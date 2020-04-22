4efdc6bbc7ce461b5843834989af1ffb58f21da1
tijanaostojic <63734074+tijanaostojic@users.noreply.github.com>
2020-04-22 18:23:09 +0200

    Update Srednji_bloker.hpp

---
+++ Odbojkaski turnir/Srednji_bloker.hpp
@@ -0,0 +1,20 @@
+#ifndef SREDNJI_BLOKER_HPP_INCLUDED
+#define SREDNJI_BLOKER_HPP_INCLUDED
+
+class SrednjiBloker
+{
+private:
+    int Blokovi;
+    int poenSmec;
+};
+
+
+
+
+
+
+
+
+
+
+#endif // SREDNJI_BLOKER_HPP_INCLUDED
